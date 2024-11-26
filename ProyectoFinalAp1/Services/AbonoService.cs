bd3ecab653c6766e456e0601c85c537b5ddd3b9a
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-26 11:09:33 -0500

    abono service

--- ProyectoFinalAp1/Services/AbonoService.cs
+++ ProyectoFinalAp1/Services/AbonoService.cs
@@ -1,6 +1,14 @@
-﻿namespace ProyectoFinalAp1.Services
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAp1.Data;
+
+namespace ProyectoFinalAp1.Services
 {
-    public class AbonoService
+    public class AbonoService(IDbContextFactory<ApplicationDbContext> DbFactory)
     {
+       public async Task <bool>Existe(int AbonoId)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.abonos.AnyAsync(a => a.AbonoId == AbonoId);
+        }
     }
 }
