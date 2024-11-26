e003084ed6e348b6873ef90296347aa50925a9e2
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-26 11:09:17 -0500

    Contexto

--- ProyectoFinalAp1/Data/ApplicationDbContext.cs
+++ ProyectoFinalAp1/Data/ApplicationDbContext.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAp1.Models;
 
-namespace ProyectoFinalAp1.Data
+namespace ProyectoFinalAp1.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {}
+    public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+         : base(options)
+    {  }
 
-    
+    public DbSet<Abonos> abonos{ get; set; }
+    public DbSet<Pagos> pagos{ get; set; }
+    public DbSet<Cobros> cobros{ get; set; }
+    public DbSet<Facturas> facturas{ get; set; }
+    public DbSet<Deudores> deudores { get; set; }
+    public DbSet<Prestamos> prestamos { get; set; }
 }
+
+
