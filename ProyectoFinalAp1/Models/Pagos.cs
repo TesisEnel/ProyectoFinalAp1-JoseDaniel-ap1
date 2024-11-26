addd350635cc151be415e18faa4c52551e291cf6
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-26 10:50:13 -0500

    Modelo Pagos

--- ProyectoFinalAp1/Models/Pagos.cs
+++ ProyectoFinalAp1/Models/Pagos.cs
@@ -8,12 +8,12 @@ public class Pagos
     [Key]
     public int PagoId { get; set; }
 
-    [Required(ErrorMessage = "Favor seleccionar un cliente.")]
-    [Range(1, int.MaxValue, ErrorMessage = "Favor seleccionar un cliente válido.")]
-    public int ClienteId { get; set; }
+    [Required(ErrorMessage = "Favor seleccionar un deudor.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Favor seleccionar un deudor válido.")]
+    public int DeudorId { get; set; }
 
-    [ForeignKey("ClienteId")]
-    public Clientes? clientes { get; set; }
+    [ForeignKey("DeudorId")]
+    public Deudores? deudores { get; set; }
 
     [Required(ErrorMessage = "El monto del pago es obligatorio.")]
     [Range(0.01, double.MaxValue, ErrorMessage = "El Capital del pago debe ser mayor a cero.")]
