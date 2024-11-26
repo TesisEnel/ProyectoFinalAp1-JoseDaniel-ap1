6fefd2bb330f1dce0870f8c29358094e1a121307
Cape23180909 <samilgamer@outlook.com>
2024-11-26 11:46:42 -0400

    Terminando de hacer el modelo de Cobros.

---
+++ ProyectoFinalAp1/Models/Cobros.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinalAp1.Models;
+public class Cobros
+{
+    [Key]
+    public int CobroId { get; set; }
+
+    [Required(ErrorMessage = "Favor seleccionar un deudor.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Favor seleccionar un deudor válido.")]
+    public int DeudorId { get; set; }
+
+    [ForeignKey("DeudorId")]
+    public Deudores? deudores { get; set; }
+
+    [Required(ErrorMessage = "Favor seleccionar un préstamo.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Favor seleccionar un préstamo válido.")]
+    public int PrestamoId { get; set; }
+
+    [ForeignKey("PrestamoId")]
+    public Prestamos? Prestamo { get; set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "La mora no puede ser un valor negativo.")]
+    public decimal? Mora { get; set; }
+
+    [Required(ErrorMessage = "El importe a pagar es obligatorio.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El importe a pagar debe ser mayor a cero.")]
+    public decimal? ImportePagar { get; set; }
+
+    [Required(ErrorMessage = "La fecha de prestamo es obligatorio.")]
+    public DateTime? FechaPrestamo { get; set; }
+
+    [Required(ErrorMessage = "La fecha de cobro es obligatorio.")]
+    public DateTime? FechaCobro { get; set; }
+}
