4147bbc501ddc0bebc36dec2c45817860b53d32f
Cape23180909 <samilgamer@outlook.com>
2024-11-30 02:52:57 -0400

    Quitando lo nulable, ya que una de esas propiedades se estaba pasando nula en la tabla de la base de datos del mismo.

--- ProyectoFinalAp1/Models/Cobros.cs
+++ ProyectoFinalAp1/Models/Cobros.cs
@@ -12,26 +12,26 @@ public class Cobros
     public int DeudorId { get; set; }
 
     [ForeignKey("DeudorId")]
-    public Deudores? deudores { get; set; }
+    public Deudores deudores { get; set; }
 
     [Required(ErrorMessage = "Favor seleccionar un préstamo.")]
     [Range(1, int.MaxValue, ErrorMessage = "Favor seleccionar un préstamo válido.")]
     public int PrestamoId { get; set; }
 
     [ForeignKey("PrestamoId")]
-    public Prestamos? Prestamo { get; set; }
+    public Prestamos Prestamo { get; set; }
 
     [Range(0, double.MaxValue, ErrorMessage = "La mora no puede ser un valor negativo.")]
     public decimal? Mora { get; set; }
 
     [Required(ErrorMessage = "El importe a pagar es obligatorio.")]
     [Range(0.01, double.MaxValue, ErrorMessage = "El importe a pagar debe ser mayor a cero.")]
-    public decimal? ImportePagar { get; set; }
+    public decimal ImportePagar { get; set; }
 
     [Required(ErrorMessage = "La fecha de prestamo es obligatorio.")]
-    public DateTime? FechaPrestamo { get; set; }
+    public DateTime FechaPrestamo { get; set; }
 
     [Required(ErrorMessage = "La fecha de cobro es obligatorio.")]
     public DateTime? FechaCobro { get; set; }
-    public virtual ICollection<Facturas> Facturas { get; set; } = new List<Facturas>();
+    //public virtual ICollection<Facturas> Facturas { get; set; } = new List<Facturas>();
 }
