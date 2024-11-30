67d106224143f3a6a5c48399a4dc867b54415a88
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-30 17:39:33 -0500

    Service garantia

---
+++ ProyectoFinalAp1/Services/GarantiaService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAp1.Data;
+using ProyectoFinalAp1.Models;
+using System.Linq.Expressions;
+
+namespace ProyectoFinalAp1.Services;
+
+public class GarantiaService(IDbContextFactory<ApplicationDbContext> DbFactory)
+{
+    public async Task<bool> Existe(int garantiaid)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.garantias.AnyAsync(a => a.GarantiaId==garantiaid);
+    }
+    private async Task<bool> Insertar(Garantias garantia)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.garantias.Add(garantia);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+    private async Task<bool> Modificar(Garantias garantia)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.garantias.Update(garantia);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+    public async Task<bool> Guardar(Garantias garantia)
+    {
+        if (!await Existe(garantia.GarantiaId))
+            return await Insertar(garantia);
+        else
+            return await Modificar(garantia);
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        var eliminado = await contexto.garantias
+            .Where(a => a.GarantiaId == id)
+            .ExecuteDeleteAsync();
+        return eliminado > 0;
+    }
+
+    public async Task<Garantias?> Buscar(int garantiaid)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.garantias
+            .FirstOrDefaultAsync(a => a.GarantiaId == garantiaid);
+    }
+    public async Task<List<Garantias>> Listar(Expression<Func<Garantias, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.garantias
+            .Where(criterio)
+            .Include(c => c.Prestamos)
+            .Include(c => c.Deudores)
+            .ToListAsync();
+    }
+    public async Task<List<Garantias>> Listargarantias()
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.garantias
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
