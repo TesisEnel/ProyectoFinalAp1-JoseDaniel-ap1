1b18b0380ad09211b8911abea12e7014c4c1ad21
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-12-02 10:00:50 -0500

    Modelo cobros

--- ProyectoFinalAp1/Models/Cobros.cs
+++ ProyectoFinalAp1/Models/Cobros.cs
@@ -21,23 +21,23 @@ public class Cobros
     [ForeignKey("PrestamoId")]
     public Prestamos Prestamo { get; set; }
 
-    //[Required(ErrorMessage = "Favor seleccionar un cobrador.")]
-    ////public int CobradorId { get; set; }
+    [Required(ErrorMessage = "Favor seleccionar un cobrador.")]
+    public int CobradorId { get; set; } // Nueva propiedad de CobradorId
 
-    ////[ForeignKey("CobradorId")]
-    ////public Cobradores Cobrador { get; set; } 
-    
+    [ForeignKey("CobradorId")]
+    public Cobradores Cobrador { get; set; } // Relación con Cobradores
+
     public decimal? Mora { get; set; }
 
     [Required(ErrorMessage = "El importe a pagar es obligatorio.")]
     [Range(0.01, double.MaxValue, ErrorMessage = "El importe a pagar debe ser mayor a cero.")]
     public decimal ImportePagar { get; set; }
 
-
     [Required(ErrorMessage = "La fecha de prestamo es obligatorio.")]
     public DateTime FechaPrestamo { get; set; }
 
     [Required(ErrorMessage = "La fecha de cobro es obligatorio.")]
     public DateTime? FechaCobro { get; set; }
-    //public virtual ICollection<Facturas> Facturas { get; set; } = new List<Facturas>();
+
+    public virtual ICollection<CobrosDetalle> CobrosDetalles { get; set; } = new List<CobrosDetalle>();
 }
