559d9c412a267ef4365d529cac7d393586902a54
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-12-02 10:01:07 -0500

    Modelo cobrosDetalle

---
+++ ProyectoFinalAp1/Models/CobrosDetalle.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinalAp1.Models;
+
+public class CobrosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    public int CobroId { get; set; }
+    [ForeignKey("CobroId")]
+    public Cobros cobros { get; set; }
+
+    public int PrestamoId { get; set; }
+
+    [ForeignKey("PrestamoId")]
+    public Prestamos Prestamo { get; set; }
+
+    public decimal ValorCobrado { get; set; }
+}
