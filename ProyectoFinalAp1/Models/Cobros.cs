8640ea1ef5707c52260e2e1b8e1d41aa836a537f
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-12-08 12:25:04 -0500

    cobros

--- ProyectoFinalAp1/Models/Cobros.cs
+++ ProyectoFinalAp1/Models/Cobros.cs
@@ -9,23 +9,20 @@ public class Cobros
     public int CobroId { get; set; }
 
     public int DeudorId { get; set; }
-
-    // Propiedades de navegación
     [ForeignKey("DeudorId")]
 
     public virtual Deudores Deudor { get; set; }
 
-    public int CobradorId { get; set; } // Nueva propiedad de CobradorId
+    public int CobradorId { get; set; } 
 
     [ForeignKey("CobradorId")]
     public Cobradores Cobrador { get; set; }
-
+    public decimal ImporteAPagar { get; set; }
     public decimal Mora { get; set; }
-    public decimal ImportePagar { get; set; }
     [Required(ErrorMessage = "El campo Fecha de Cobro es obligatorio.")]
     public DateTime FechaPrestamo { get; set; }
     public DateTime FechaCobro { get; set; }
 
-    // Relación uno a muchos con CobrosDetalle
+
     public List<CobrosDetalle> CobrosDetalles { get; set; } = new List<CobrosDetalle>();
 }
